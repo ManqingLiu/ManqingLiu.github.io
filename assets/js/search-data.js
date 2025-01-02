f778fba5e440115a75d45ec3a793c626886ca4a1
ManqingLiu <75640131+ManqingLiu@users.noreply.github.com>
2025-01-02 00:31:23 +0000

    Deploying to gh-pages from @ ManqingLiu/ManqingLiu.github.io@fcae772560e33683b6bd8abe22d170eecd327e40 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%6E%71%69%6E%67%6C%69%75@%67.%68%61%72%76%61%72%64.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/ManqingLiu", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -500,6 +507,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/Manqing Liu", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,14 +527,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=1EijlwwAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
