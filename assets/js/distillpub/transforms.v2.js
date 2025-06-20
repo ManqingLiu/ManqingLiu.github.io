fcfdaf961306c24dd8eee1cd70bdd67b71b84a6d
Manqing Liu <manqingliu@g.harvard.edu>
2025-06-19 22:03:02 -0400

    changed font

--- assets/js/distillpub/transforms.v2.js
+++ assets/js/distillpub/transforms.v2.js
@@ -4383,7 +4383,7 @@
                   fontName: "SansSerif-Regular",
                 },
                 mathtt: {
-                  variant: "monospace",
+                  variant: "Garamond",
                   fontName: "Typewriter-Regular",
                 },
               };
